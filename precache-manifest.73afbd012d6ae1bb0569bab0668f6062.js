a0ef89664ec750f58c5ae899949c0e36f87b6a53
Andy Martinez <andymartinez@Andys-MacBook-Pro.local>
2019-07-27 12:55:22 -0400

    Updates

---
+++ precache-manifest.73afbd012d6ae1bb0569bab0668f6062.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "545bb5149a289f9c3a9ab50acac7130f",
+    "url": "/todo-app-react-hooks/index.html"
+  },
+  {
+    "revision": "ac29b0b47897718098a0",
+    "url": "/todo-app-react-hooks/static/css/main.a16aa873.chunk.css"
+  },
+  {
+    "revision": "eb8b6cd8e42a9f2b7950",
+    "url": "/todo-app-react-hooks/static/js/2.db009984.chunk.js"
+  },
+  {
+    "revision": "ac29b0b47897718098a0",
+    "url": "/todo-app-react-hooks/static/js/main.ae53d579.chunk.js"
+  },
+  {
+    "revision": "0526fded354160c4aa4b",
+    "url": "/todo-app-react-hooks/static/js/runtime~main.a3e4e470.js"
+  }
+]);
